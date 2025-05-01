87d856d71300d1baca9dc21ff60a88b9c4c4ff60
muflih1 <muflimuflih306@gmail.com>
2025-05-02 00:50:26 +0530

    fix: cors error

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,13 +3,13 @@ import axios from "axios"
 const API_KEY = import.meta.env.VITE_APP_API_KEY;
 
 export const searchMoviesByTitle = async (query: string) => {
-  const { data } = await axios.get(`http://www.omdbapi.com/?apikey=${API_KEY}&s=${encodeURIComponent(query)}`)
+  const { data } = await axios.get(`https://www.omdbapi.com/?apikey=${API_KEY}&s=${encodeURIComponent(query)}`)
 
   return data
 }
 
 export const getMovieById = async (id: string) => {
-  const { data } = await axios.get(`http://www.omdbapi.com/?apikey=${API_KEY}&i=${id}`)
+  const { data } = await axios.get(`https://www.omdbapi.com/?apikey=${API_KEY}&i=${id}`)
 
   return data
 }
